9c943bfe003c792a0a94c7502fe88fe44aa8f4f1
Randolph Fabrizio Ramirez Palacios <randolphpeluche@gmail.com>
2024-01-16 14:03:46 -0500

    more_details

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -25,8 +25,12 @@ const Hero = () => {
             </div>
             <h1 className="h1 mb-4">Hello, my name is Randolph </h1>
             <p className="subtitle max-w-[490px] mx-auto xl:mx-0">
-              Brief description with insights into myself, my vocational
-              journey, and what I engage in professionally
+              Dedicated Systems Engineering student diving into web development
+              since 2019. Comfortable with HTML, CSS, JavaScript, TypeScript,
+              PHP, SQL, and more. Familiar with frameworks like React, Node.js
+              Express.js, Next.js, Flask, and versed in Git and GitHub.
+              Embracing continuous learning and collaboration, I see software
+              development as an ongoing, humble journey.
             </p>
             {/* buttons */}
             <div className="flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12">
@@ -47,14 +51,13 @@ const Hero = () => {
           </div>
           {/* image */}
           <div className="hidden xl:flex relative">
-            {/* badge 1 */}
-            <Badge
+            {/* <Badge
               containerStyles="absolute top-[24%] -left-[5rem]"
               icon={<RiBriefcase4Fill />}
               endCountNum={3}
               badgeText="Years of experience"
-            />
-            {/* badge 2 */}
+            /> */}
+
             <Badge
               containerStyles="absolute top-[80%] -left-[1rem]"
               icon={<RiTodoFill />}
@@ -62,14 +65,14 @@ const Hero = () => {
               // endCountText='k'
               badgeText="Finished Projects"
             />
-            {/* badge 3 */}
-            <Badge
+
+            {/* <Badge
               containerStyles="absolute top-[55%] -right-8"
               icon={<RiTeamFill />}
               endCountNum={9}
               // endCountText='k'
               badgeText="Happy Clients"
-            />
+            /> */}
             <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2"></div>
             <DevImg
               containerStyles="bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom"
