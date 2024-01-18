3bfdc0c37592cac04e85b4bc5cc2ce324f30fc47
Randolph Fabrizio Ramirez Palacios <randolphpeluche@gmail.com>
2024-01-18 14:31:08 -0500

    little_changes

--- app/page.jsx
+++ app/page.jsx
@@ -2,16 +2,18 @@ import About from "@/components/About";
 import Hero from "@/components/Hero";
 import Reviews from "@/components/Reviews";
 import Services from "@/components/Services";
+import Achievements from "@/components/Achievements";
 import Works from "@/components/Works";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <About/>
+      <About />
       {/* <Services/> */}
-      <Works/>
-      <Reviews/>
+      <Achievements />
+      <Works />
+      <Reviews />
     </main>
   );
 }
--- components/Achievements.jsx
+++ components/Achievements.jsx
@@ -18,7 +18,7 @@ import ProjectCard from "@/components/ProjectCard";
 const projectData = [
   {
     image: "/work/3.png",
-    category: "react js",
+    category: "experience",
     name: "Nexa Website",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sed?",
@@ -27,7 +27,7 @@ const projectData = [
   },
   {
     image: "/work/4.png",
-    category: "react js",
+    category: "experience",
     name: "Solstice Website",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sed?",
@@ -36,7 +36,7 @@ const projectData = [
   },
   {
     image: "/work/2.png",
-    category: "next js",
+    category: "achievements",
     name: "Lumina Website",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sed?",
@@ -45,53 +45,8 @@ const projectData = [
   },
   {
     image: "/work/1.png",
-    category: "next js",
+    category: "achievements",
     name: "Evolve Website",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sed?",
-    link: "/",
-    github: "/",
-  },
-  {
-    image: "/work/3.png",
-    category: "next js",
-    name: "Ignite Website",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sed?",
-    link: "/",
-    github: "/",
-  },
-  {
-    image: "/work/4.png",
-    category: "next js",
-    name: "Envision Website",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sed?",
-    link: "/",
-    github: "/",
-  },
-  {
-    image: "/work/1.png",
-    category: "fullstack",
-    name: "Serenity Website",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sed?",
-    link: "/",
-    github: "/",
-  },
-  {
-    image: "/work/3.png",
-    category: "fullstack",
-    name: "Nova Website",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sed?",
-    link: "/",
-    github: "/",
-  },
-  {
-    image: "/work/2.png",
-    category: "fullstack",
-    name: "Zenith Website",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sed?",
     link: "/",
