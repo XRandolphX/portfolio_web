478cdd3c66b558bf7d5e5ac174e6bf50d49409f5
Randolph Fabrizio Ramirez Palacios <randolphpeluche@gmail.com>
2024-01-22 15:22:56 -0500

    contact_completed

--- components/FormElement.jsx
+++ components/FormElement.jsx
@@ -5,22 +5,52 @@ import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
 import { User, ArrowRightIcon, MessageSquare, MailIcon } from "lucide-react";
 
+// for EmailJS to work
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+// Importing Keys
+import KEYS from "../app/keys";
+
 const FormElement = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        KEYS.SERVICE_ID,
+        KEYS.TEMPLATE_ID,
+        form.current,
+        KEYS.PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+          e.target.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
-    <form className="flex flex-col gap-y-4">
+    <form ref={form} onSubmit={sendEmail} className="flex flex-col gap-y-4">
       {/* input */}
       <div className="relative flex items-center">
-        <Input type="name" id="name" placeholder="Name" />
+        <Input type="name" id="name" placeholder="Name" name="user_name" />
         <User className="absolute right-6" size={20} />
       </div>
       {/* input */}
       <div className="relative flex items-center">
-        <Input type="email" id="email" placeholder="Email" />
+        <Input type="email" id="email" placeholder="Email" name="user_email" />
         <MailIcon className="absolute right-6" size={20} />
       </div>
       {/* textarea */}
       <div className="relative flex items-center">
-        <Textarea placeholder="Type Your Message Here" />
+        <Textarea placeholder="Type Your Message Here" name="message" />
         <MessageSquare className="absolute top-4 right-6" size={20} />
       </div>
       <Button className="flex items-center gap-x-1 max-w-[166px]">
