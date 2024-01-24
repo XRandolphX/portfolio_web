675a559a424bd31067710e4608ab23c1fe0d6f7a
Randolph Fabrizio Ramirez Palacios <randolphpeluche@gmail.com>
2024-01-23 22:21:25 -0500

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -39,7 +39,11 @@ const Hero = () => {
                   Contact me <Send size={18} />
                 </Button>
               </Link>
-              <Link href="https://drive.google.com/file/d/1G3QHg_WWYxm9RiauG6kOn_fAvfoXNAYx/view?usp=drive_link">
+              <Link
+                href="https://drive.google.com/file/d/1G3QHg_WWYxm9RiauG6kOn_fAvfoXNAYx/view?usp=drive_link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <Button variant="secondary" className="gap-x-2">
                   Download CV <Download size={18} />
                 </Button>
