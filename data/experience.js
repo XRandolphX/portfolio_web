78a7c237f1996e8dc59f2f5f8b680c2862fa8ceb
XRandolphX <randolphfrp@gmail.com>
2026-06-16 15:38:57 -0500

    refactor: extract qualifications data to data/experience.js

---
+++ data/experience.js
@@ -0,0 +1,52 @@
+export const qualificationData = [
+  {
+    title: "education",
+    data: [
+      {
+        university: "Cesar Vallejo University",
+        qualification: "Systems Engineer",
+        years: "2019 - 2024",
+      },
+      {
+        university: "ICPNA",
+        qualification: "English Student",
+        years: "2017 - 2020",
+      },
+    ],
+  },
+  {
+    title: "experience",
+    data: [
+      {
+        company: "Fe y Alegría 18",
+        qualification: "IT & Programming Instructor",
+        years: "October - November 2025",
+      },
+      {
+        company: "Fe y Alegría 18",
+        qualification: "Educational Technology Specialist",
+        years: "June - September 2025",
+      },
+      {
+        company: "Inngresa",
+        qualification: "Junior Technical Writer",
+        years: "October - December 2024",
+      },
+      {
+        company: "UGEL Sullana",
+        qualification: "IT Support Assistant (Pre-Professional Internship)",
+        years: "February - June 2024",
+      },
+      {
+        company: "Ricardo Palma School",
+        qualification: "Software Developer (Pre-Professional Internship)",
+        years: "September - December 2023",
+      },
+      {
+        company: "Rosa Mistica School",
+        qualification: "Computer Science Teacher & Administrative Manager",
+        years: "May 2022 - December 2023",
+      },
+    ],
+  },
+];
