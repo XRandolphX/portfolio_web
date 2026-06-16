3aca0e69afa114abaf4481e7fe9cff4c30868f62
XRandolphX <randolphfrp@gmail.com>
2026-06-16 15:49:45 -0500

    refactor(About): import qualificationData from data layer

--- components/About.jsx
+++ components/About.jsx
@@ -11,6 +11,8 @@ import {
   Briefcase,
 } from "lucide-react";
 
+import { qualificationData } from "@/data/experience";
+
 const infoData = [
   {
     icon: <User2 size={20} />,
@@ -31,59 +33,6 @@ const infoData = [
   {
     icon: <HomeIcon size={20} />,
     text: "Sullana - Piura - Peru",
-  },
-];
-
-const qualificationData = [
-  {
-    title: "education",
-    data: [
-      {
-        university: "Cesar Vallejo University",
-        qualification: "System Engineer Student",
-        years: "2019 - 2024",
-      },
-      {
-        university: "ICPNA",
-        qualification: "English Student",
-        years: "2017 - 2020",
-      },
-    ],
-  },
-  {
-    title: "experience",
-    data: [
-      {
-        company: "Fe y Alegría 18",
-        qualification: "IT & Programming Instructor",
-        years: "October - November 2025",
-      },
-      {
-        company: "Fe y Alegría 18",
-        qualification: "Educational Technology Specialist",
-        years: "June - September 2025",
-      },
-      {
-        company: "Inngresa",
-        qualification: "Junior Technical Writer",
-        years: "October - December 2024",
-      },
-      {
-        company: "UGEL Sullana",
-        qualification: "IT Support Assistant (Pre-Professional Internship)",
-        years: "February - June 2024",
-      },
-      {
-        company: "Ricardo Palma School",
-        qualification: "Software Developer (Pre-Professional Internship)",
-        years: "September 2023 - December 2023",
-      },
-      {
-        company: "Rosa Mistica School",
-        qualification: "Computer Science Teacher & Administrative Manager",
-        years: "May 2022 - December 2023",
-      },
-    ],
   },
 ];
 
